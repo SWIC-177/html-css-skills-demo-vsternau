e938018f2fc479aa64779f2f50a9542fa8399afa
vsternau <victoria.sternau@student.swic.edu>
2024-05-07 21:10:22 -0500

    Create const formEls to select input and comments

---
+++ main.js
@@ -0,0 +1,19 @@
+/**
+ * TODO:
+ * 1. 'Select' all of the input fields.
+ * 2. 'Select' the submit button.
+ * 3. Add event listeners to each input field, such that
+ *  upon losing focus (`blur` event) on the input fields, we will
+ * show the error. For now, we are not validating the input fields.
+ */
+
+const formEls = [
+    ...Array.from(document.querySelectorAll("input")),
+    document.querySelector("#comments"),
+];
+
+console.log(formEls);
+
+const submitBtn = document.querySelector("button");
+
+console.log(submitBtn);
