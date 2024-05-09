ff778bfb7f32499889c718b453ba61e0c09f2cf8
vsternau <victoria.sternau@student.swic.edu>
2024-05-08 20:40:50 -0500

    Create validation for "email" and "comments"

--- src/lib.js
+++ src/lib.js
@@ -3,16 +3,25 @@ export const ERRORS = [
         id: "firstname",
         msg: "First name is required",
         validate(val) {
-            return val.length > 10 && val.includes("  ");
+            return val.length > 10;
         },
     },
     { 
         id: "lastname",
         msg: "Last name is required",
         validate(val) {
-            return val.length > 15 && val.includes("  ");
+            return val.length > 15;
         },
     },
-    { id: "email", msg: "Email address is required" },
-    { id: "comments", msg: "Please enter a message between 10 and 100 characters" },
+    { id: "email", msg: "Email address is required",
+    validate(val) {
+        return val.includes("@");
+    },
+     },
+    { id: "comments", 
+    msg: "Please enter a message between 10 and 100 characters",
+    validate(val) {
+    return val.length > 10 && val.length < 100;
+},
+}
 ];
